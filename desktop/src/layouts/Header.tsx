32116efcde75424d8409b9c54163d86ae04a8d11
trrueee <luokk0349@gmail.com>
2026-06-11 03:00:58 +0800

    add desktop header layout

---
+++ desktop/src/layouts/Header.tsx
@@ -0,0 +1,13 @@
+export default function Header() {
+  return (
+    <header className="app-header">
+      <b>数据库可视化 + 智能问数</b>
+      <nav aria-label="主导航">
+        <button className="on">工作台</button>
+        <button>数据库</button>
+        <button>智能问数</button>
+      </nav>
+      <span>A</span>
+    </header>
+  );
+}
